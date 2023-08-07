ac0c69d923b2badb9a2befa49b56cd0d46e5ba87
Patrício Osterno Rios <patricio.osterno1@gmail.com>
2023-08-07 16:26:47 -0300

    .

--- GeCli.Manager.Tests/CustomerManagerTest.cs
+++ GeCli.Manager.Tests/CustomerManagerTest.cs
@@ -2,9 +2,12 @@
 using GeCli.Back._FakeData.CustomerData;
 using GeCli.Back.Domain.Entities.Customers;
 using GeCli.Back.Domain.Interfaces;
+using GeCli.Back.Manager.Implementation;
 using GeCli.Back.Manager.Interfaces;
+using GeCli.Back.Manager.Mappings;
 using GeCli.Back.Shared.ModelView.Customer;
 using NSubstitute;
+using Xunit;
 
 namespace GeCli.Manager.Tests;
 
@@ -23,5 +26,26 @@ public class CustomerManagerTest
     public CustomerManagerTest()
     {
         _customerRepository = Substitute.For<ICustomerRepository>();
+        _mapper = new MapperConfiguration(p => p.AddProfile<CustomerMappingProfile>()).CreateMapper();
+        _customerManager = new CustomerManager(_customerRepository,_mapper);
+        _customerFake = new CustomerFake();
+        _newCustomerFake = new NewCustomerFake();
+
+        _customer = _customerFake.Generate();
+        _newCustomer = _newCustomerFake.Generate();
+        _updateCustomer = _updateCustomerFake.Generate();
+    }
+
+    //Corrigir
+    [Fact]
+    public async Task GetCustomers_Ok()
+    {
+        var listCustomer = _customerFake.Generate(10);
+        _customerRepository.GetCustomersAsync().Returns(listCustomer);
+        var control = _mapper.Map<IEnumerable<Customer>, IEnumerable<CustomerView>>(listCustomer);
+        var returnResult = await _customerManager.GetCustomersAsync();
+
+        await _customerRepository.Received().GetCustomersAsync();
+        returnResult.Should().BeEquivalentTo(control);
     }
 }
